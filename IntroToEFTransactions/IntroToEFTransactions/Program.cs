04a75a720428706cc1e8a0ca740f453d6764831a
Tiller <ctiller@proforma.com>
2018-04-16 15:47:02 -0400

    Add query to get all today transactions

--- IntroToEFTransactions/IntroToEFTransactions/Program.cs
+++ IntroToEFTransactions/IntroToEFTransactions/Program.cs
@@ -23,10 +23,134 @@ namespace IntroToEFTransactions
                 AmountChanged = 300.30m,
                 NewAmount = 555.65m
             };
-            dbContext.EFTransactions.Add(t1);
+
+            var t2 = new EFTransactions
+            {
+                Timestamp = DateTime.Now,
+                Action = "Withdraw",
+                AccountNumber = 8765,
+                AmountChanged = 4332.11m,
+                NewAmount = 2554.43m
+            };
+            var t3 = new EFTransactions
+            {
+                Timestamp = DateTime.Now,
+                Action = "Withdraw",
+                AccountNumber = 8767,
+                AmountChanged = 90.01m,
+                NewAmount = 10000.99m
+            };
+            var t4 = new EFTransactions
+            {
+                Timestamp = DateTime.Now,
+                Action = "Withdraw",
+                AccountNumber = 8768,
+                AmountChanged = 1250.23m,
+                NewAmount = 854.42m
+            };
+            var t5 = new EFTransactions
+            {
+                Timestamp = DateTime.Now,
+                Action = "Withdraw",
+                AccountNumber = 8761,
+                AmountChanged = 578.92m,
+                NewAmount = 1111.86m
+            };
+
+            var t6 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 12),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t7 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 13),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t8 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 14),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t9 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 15),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t10 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 16),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t11 = new EFTransactions
+            {
+                Timestamp = new DateTime(2012, 10, 17),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+            var t12 = new EFTransactions
+            {
+                Timestamp = new DateTime(2020, 10, 18),
+                Action = "Deposit",
+                AccountNumber = 66441,
+                AmountChanged = 778.42m,
+                NewAmount = 99975.43m
+            };
+
+
+
+            //dbContext.EFTransactions.Add(t1);
+            //dbContext.EFTransactions.Add(t2);
+            //dbContext.EFTransactions.Add(t3);
+            //dbContext.EFTransactions.Add(t4);
+            //dbContext.EFTransactions.Add(t5);
+            dbContext.EFTransactions.Add(t6);
+            dbContext.EFTransactions.Add(t7);
+            dbContext.EFTransactions.Add(t8);
+            dbContext.EFTransactions.Add(t9);
+            dbContext.EFTransactions.Add(t10);
+            dbContext.EFTransactions.Add(t11);
+            dbContext.EFTransactions.Add(t12);
 
             // save transactions to db.
+            //dbContext.SaveChanges();
             dbContext.SaveChanges();
+
+            // SELECT
+
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+            var TodayTransactions = dbContext.EFTransactions.Where( date => today < date.Timestamp && tomorrow > date.Timestamp);
+
+            //foreach (var transaction in TodayTransactions)
+            //{
+            //    Console.WriteLine(transaction.Timestamp);
+            //    Console.WriteLine(DateTime.Today);
+            //    Console.WriteLine(transaction.Timestamp > DateTime.Today);
+            //}
+
+            foreach (var transaction in TodayTransactions)
+            {
+                Console.WriteLine(transaction.Timestamp);
+            }
+            Console.ReadLine();
         }
     }
 }
